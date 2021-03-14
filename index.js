3c004197091feb34a36e1bc14ce6e5680fc811ff
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-14 20:30:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,31 @@ function mapToSquare(sourceArray){
   return sourceArray.map(x => x ** 2);
 }
 
+function reduceToTotal(sourceArray, startingPoint = 0){
+    const reducer = function(accumulator, currentValue){ return accumulator + currentValue };
+
+    return sourceArray.reduce(reducer, startingPoint);
+}
+
+function reduceToAllTrue(sourceArray){
+
+    const reducer = function(accumulator, currentValue){  
+        if(!!accumulator == true && !!currentValue == true){
+            return true;
+        } else {
+            return false;
+        }
+    };
+    return sourceArray.reduce(reducer, true);
+}
+
+function reduceToAnyTrue(sourceArray){
+    const reducer = function(accumulator, currentValue){  
+        if(accumulator == true){
+            return true;
+        } else {
+            return !!currentValue;
+        } 
+    };
+    return sourceArray.reduce(reducer, false);
+}

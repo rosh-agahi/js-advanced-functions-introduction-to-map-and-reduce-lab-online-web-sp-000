334c7f26ed267bfb38f444109554d474d5c9c932
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-14 20:27:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ function mapToNegativize(sourceArray){
   return sourceArray.map(x => x * -1);
 }
 
-function mapToNegativize(sourceArray){
-  return sourceArray.map(x => x * -1);
+function mapToNoChange(sourceArray){
+  return sourceArray.map(x => x * 1);
+}
+
+function mapToNoChange(sourceArray){
+  return sourceArray.map(x => x * 1);
+}
+
+function mapToNoChange(sourceArray){
+  return sourceArray.map(x => x * 1);
 }
